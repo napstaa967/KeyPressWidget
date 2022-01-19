907531c43f502aa56d9ce8a0e722b12ebe83a132
TwoPTwentySix <57607350+napstaa967@users.noreply.github.com>
2022-01-19 15:52:41 -0600

    key monitor list being broken

--- Form3.cs
+++ Form3.cs
@@ -1,179 +1,182 @@
-﻿using System;
-using System.Runtime.InteropServices;
-using System.Text.RegularExpressions;
-using System.Collections.Generic;
-using System.Windows.Forms;
-using Gma.System.MouseKeyHook;
 using keystuff.Properties;
+using Microsoft.Win32;
+using RegistryUtils;
+using System;
+using System.Collections.Generic;
 using System.Drawing;
+using System.Linq;
+using System.Threading;
+using System.Windows.Forms;
 
 namespace keystuff
 {
-    public partial class Form3 : Form
+    partial class Form3
     {
+        public string ThemeSet = Settings.Default.Theme;
+        public Color ThemeTextColor;
+        public Color ThemeBackColor;
+        int regsys = (int)Registry.GetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize",
+            "AppsUseLightTheme",
+            0);
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
-        
-        public static string lastkeypress;
-        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
-        private static extern IntPtr CreateRoundRectRgn
-        (
-            int nLeftRect,     // x-coordinate of upper-left corner
-            int nTopRect,      // y-coordinate of upper-left corner
-            int nRightRect,    // x-coordinate of lower-right corner
-            int nBottomRect,   // y-coordinate of lower-right corner
-            int nWidthEllipse, // width of ellipse
-            int nHeightEllipse // height of ellipse
-        );
-        private IKeyboardMouseEvents m_GlobalHook;
-        public ContextMenu contextMenu1;
-        public MenuItem alwaysOnTop;
-        public MenuItem exit;
-        public MenuItem autoHide;
-        public MenuItem wordBuild;
-        public MenuItem settingStuff;
-        public bool CurrentlyPressed = false;
-        public string monitorkey;
-        public Form3(string thekey)
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
         {
-            if (ThemeSet == "System")
+            if (disposing && (components != null))
             {
-                if (regsys == 1)
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        /// 
+        public RegistryMonitor monitor;
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label1.Font = global::keystuff.Properties.Settings.Default.Font;
+            this.label1.ForeColor = global::keystuff.Properties.Settings.Default.ThemeTextColor;
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(panel1.Width, panel1.Height);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "[Idle]";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form3_MouseDown);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 5000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Font = global::keystuff.Properties.Settings.Default.Font;
+            this.panel1.ForeColor = global::keystuff.Properties.Settings.Default.ThemeTextColor;
+            this.panel1.Location = new System.Drawing.Point(20, 20);
+            this.panel1.Name = "panel1";
+            this.panel1.TabIndex = 1;
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = global::keystuff.Properties.Settings.Default.ThemeBackColor;
+            this.ClientSize = new System.Drawing.Size(300, 300);
+            this.Controls.Add(this.panel1);
+            this.DataBindings.Add(new System.Windows.Forms.Binding("TopMost", global::keystuff.Properties.Settings.Default, "isAlwaysOnTop", true, System.Windows.Forms.DataSourceUpdateMode.OnPropertyChanged));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Location = global::keystuff.Properties.Settings.Default.Location;
+            this.Name = "Form3";
+            this.Opacity = global::keystuff.Properties.Settings.Default.Opacity;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Form3";
+            this.TopMost = global::keystuff.Properties.Settings.Default.isAlwaysOnTop;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form3_FormClosing);
+            this.Size = Settings.Default.KeyMonitorWindowSize;
+            this.panel1.Size = new System.Drawing.Size(this.Width - 40, this.Height - 40);
+            this.label1.Size = this.panel1.Size;
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+        public int reginit = (int)Registry.GetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize",
+            "AppsUseLightTheme",
+            0);
+        public int reg2;
+        private void RegistryChanged(object sender, EventArgs e)
+        {
+            reg2 = (int)Registry.GetValue("HKEY_CURRENT_USER\\Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize",
+            "AppsUseLightTheme",
+            0);
+            if (reginit != reg2)
+            {
+                if (Settings.Default.Theme == "System")
                 {
-                    ThemeSet = "System";
+                    if (reg2.ToString() == "1")
+                    {
+                        Settings.Default.ThemeTextColor = Color.Black;
+                        Settings.Default.ThemeBackColor = Color.White;
+                        Settings.Default.ThemeHighlightColor = Color.FromArgb(195, 195, 195);
+                        Settings.Default.Save();
+                    }
+                    if (reg2.ToString() == "0")
+                    {
+                        Settings.Default.ThemeTextColor = Color.White;
+                        Settings.Default.ThemeBackColor = Color.Black;
+                        Settings.Default.ThemeHighlightColor = Color.FromArgb(60, 60, 60);
+                        Settings.Default.Save();
+                    }
+                }
+                if (Settings.Default.Theme == "Light")
+                {
                     Settings.Default.ThemeTextColor = Color.Black;
                     Settings.Default.ThemeBackColor = Color.White;
-                    Settings.Default.ThemeHighlightColor = Color.FromArgb(220, 220, 220);
+                    Settings.Default.ThemeHighlightColor = Color.FromArgb(195, 195, 195);
                     Settings.Default.Save();
                 }
-                if (regsys == 0)
+                else if (Settings.Default.Theme == "Dark")
                 {
-                    ThemeSet = "System";
                     Settings.Default.ThemeTextColor = Color.White;
                     Settings.Default.ThemeBackColor = Color.Black;
                     Settings.Default.ThemeHighlightColor = Color.FromArgb(60, 60, 60);
                     Settings.Default.Save();
                 }
             }
-            if (ThemeSet == "Light")
-            {
-                Settings.Default.ThemeTextColor = Color.Black;
-                Settings.Default.ThemeBackColor = Color.White;
-                Settings.Default.ThemeHighlightColor = Color.FromArgb(220, 220, 220);
-                Settings.Default.Save();
-            }
-            else if (ThemeSet == "Dark")
-            {
-                Settings.Default.ThemeTextColor = Color.White;
-                Settings.Default.ThemeBackColor = Color.Black;
-                Settings.Default.ThemeHighlightColor = Color.FromArgb(60, 60, 60);
-                Settings.Default.Save();
-            }
-            InitializeComponent();
-            this.label1.Text = thekey;
-            this.monitorkey = thekey;
-            this.SetStyle(ControlStyles.SupportsTransparentBackColor, true);
-            this.contextMenu1 = new ContextMenu();
-            this.alwaysOnTop = new MenuItem();
-            this.wordBuild = new MenuItem();
-            this.autoHide = new MenuItem();
-            this.settingStuff = new MenuItem();
-            this.exit = new MenuItem();
-            this.FormBorderStyle = FormBorderStyle.None;
-            Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, Width, Height, 20, 20));
-            this.panel1.Region = System.Drawing.Region.FromHrgn(CreateRoundRectRgn(0, 0, this.panel1.Width, this.panel1.Height, 20, 20));
-            m_GlobalHook = Hook.GlobalEvents();
-            m_GlobalHook.KeyDown += UpdateText;
-            m_GlobalHook.KeyUp += StopKey;
-            timer2.Start();
-            
+            Form3.RestartApp();
         }
 
-        private void UpdateText(object sender, KeyEventArgs e)
+
+        private void Form3_FormClosing(object sender, System.Windows.Forms.FormClosingEventArgs e)
         {
-            
-            Show();
-            
-            if (e.KeyData.ToString() == monitorkey)
-            {
-                Console.WriteLine("shit");
-                CurrentlyPressed = true;
-                if (Opacity != Settings.Default.Opacity)
-                {
-                    timer1.Stop();
-                    Opacity = Settings.Default.Opacity;
-                }
-                Console.WriteLine(panel1.BackColor);
-                this.panel1.BackColor = Settings.Default.ThemeHighlightColor;
-                Console.WriteLine(panel1.BackColor);
-            }
+            Settings.Default.Location = this.Location;
+
+            Settings.Default.LastKeyPress = "null";
+
+            Settings.Default.Save();
         }
 
-        private void StopKey(object sender, KeyEventArgs e)
-        {
-            Show();
-            
-            if (e.KeyData.ToString() == monitorkey)
-            {
-                CurrentlyPressed = false;
-                this.panel1.BackColor = Settings.Default.ThemeBackColor;
-                if (Opacity != Settings.Default.Opacity)
-                {
-                    timer1.Stop();
-                    Opacity = Settings.Default.Opacity;
-                }
-            }
-            timer2.Start();
-        }
+        #endregion
 
-        internal static void RestartApp()
-        {
-            Application.Restart();
-        }
-
-        public const int WM_NCLBUTTONDOWN = 0xA1;
-        public const int HT_CAPTION = 0x2;
-
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
-        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
-        [System.Runtime.InteropServices.DllImport("user32.dll")]
-        public static extern bool ReleaseCapture();
-
-
-        private void Form3_MouseDown(object sender, MouseEventArgs e)
-        {
-            if (e.Button == MouseButtons.Left)
-            {
-                ReleaseCapture();
-                SendMessage(Handle, WM_NCLBUTTONDOWN, HT_CAPTION, 0);
-            }
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-            Opacity -= 0.01D;
-            if (Opacity <= 0D)
-            {
-                Opacity = 0D;
-                Hide();
-                timer1.Stop();
-            }
-        }
-        private void timer2_Tick(object sender, EventArgs e)
-        {
-            timer2.Stop();
-            if (Settings.Default.Autohide == true)
-                timer1.Start();
-        }
-
-        private void KeyboardDisplay_MouseDoubleClick(object sender, MouseEventArgs e)
-        {
-            Opacity = 0.65;
-            Show();
-        }
-
-        private void label1_Click(object sender, EventArgs e)
-        {
-
-        }
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NotifyIcon KeyboardDisplay;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Panel panel1;
     }
 }
+
